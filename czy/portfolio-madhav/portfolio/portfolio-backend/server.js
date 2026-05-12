d88404cb270c7cc5bf75c7fe0d504e82d42d4989
Madhav Bhandari <bhandari77333@gmail.com>
2026-05-12 17:24:02 +0530

    gtfyc

--- czy/portfolio-madhav/portfolio/portfolio-backend/server.js
+++ czy/portfolio-madhav/portfolio/portfolio-backend/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected ✅')
-    app.listen(PORT, () =>
+    app.listen(PORT, '0.0.0.0', () =>
       console.log(`Server running on port ${PORT}`)
     )
   })
